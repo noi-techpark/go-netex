8b34c7f5e704a411ee3260fa421486e751eb0a64
Clemens Zagler <c.zagler@noi.bz.it>
2025-06-23 15:46:03 +0200

    test: add test for Fleet/members empty/nonempty render

---
+++ model_test.go
@@ -0,0 +1,28 @@
+package netex_test
+
+import (
+	"encoding/xml"
+	"strings"
+	"testing"
+
+	"github.com/noi-techpark/go-netex"
+)
+
+func TestFleetMembers_Empty(t *testing.T) {
+	fleet := netex.Fleet{
+		Id:      netex.NewId("test"),
+		Version: "1.0",
+	}
+	fleet.Members = netex.Just([]netex.Ref{netex.NewRef("test", "test", "version")})
+
+	b, _ := xml.Marshal(fleet)
+	if !strings.Contains(string(b), "members") {
+		t.Error("xml doesn't contain members even though it should", string(b))
+	}
+
+	fleet.Members = nil
+	b, _ = xml.Marshal(fleet)
+	if strings.Contains(string(b), "members") {
+		t.Error("xml contains members even though it shouldn't", string(b))
+	}
+}
